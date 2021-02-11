e0147853e04fff07cdc27546681043ee1c60e9a2
fiontanyf <fiontanyf@gmail.com>
2021-02-11 21:13:00 +0000

    Update task-3.1.5.js
    var

---
+++ module-3/lesson-1/scripts/task-3.1.5.js
@@ -0,0 +1,14 @@
+var firstName = 'Fion';
+
+var lastName = 'Tan';
+
+fullName = firstName + lastName;
+
+console.log(fullName);
+
+var firstName = 'Fion'
+
+var lastName = 'Tan'
+
+console.log(firstName += lastName);
+
