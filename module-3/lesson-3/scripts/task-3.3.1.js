a3a185bd1a43107dedb2c45785a41bc9e2492e6c
fiontanyf <fiontanyf@gmail.com>
2021-02-18 19:22:18 +0000

    Update task-3.3.1.js
    function percentage math

---
+++ module-3/lesson-3/scripts/task-3.3.1.js
@@ -0,0 +1,8 @@
+function percentageCalculator (number, percentage){
+    var percentageOf = (number * percentage) / 100;
+    return percentageOf;
+}
+
+console.log (percentageCalculator(135,30));
+
+
