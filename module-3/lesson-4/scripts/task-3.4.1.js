a8863c027e83189430b09463c33473468ab85c0c
fiontanyf <fiontanyf@gmail.com>
2021-02-23 19:35:49 +0000

    task for loop
    add for loop for times table

---
+++ module-3/lesson-4/scripts/task-3.4.1.js
@@ -0,0 +1,23 @@
+/*
+for ( begin; condition; step ){
+    loop body
+
+}
+*/
+
+
+for ( var index = 1; index <=12; index++) {
+    console.log( index );
+    var result = index * 7;
+    console.log ( `${index} * 7 = ${result} `);
+
+    var multiply = 9;
+    var result2 = index * multiply;
+
+    console.log( `${index} * ${multiply} = ${result2}`);
+
+
+
+
+
+}
