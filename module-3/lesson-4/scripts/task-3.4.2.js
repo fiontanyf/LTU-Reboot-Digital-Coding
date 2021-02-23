b416f1a9710b5b8655a7fb9cd90f85c88e67a266
fiontanyf <fiontanyf@gmail.com>
2021-02-23 20:12:49 +0000

    update js
    add array and loop

--- module-3/lesson-4/scripts/task-3.4.2.js
+++ module-3/lesson-4/scripts/task-3.4.2.js
@@ -1,3 +1,9 @@
 let favouriteFruits = ['cherry' , 'apple' , 'avocado', 'banana' , 'durian'];
 
+favouriteFruits.push ('orange');
+
 console.log( favouriteFruits[2] );
+
+console.log( favouriteFruits );
+
+console.log( favouriteFruits.length);
---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,17 @@
+let favouriteFruits = [
+    'cherry' ,
+    'apple' ,
+    'avocado',
+    'banana' ,
+    'durian',
+    'mango'
+
+];
+
+for( let index = 0; index < favouriteFruits.length; index++ ){
+    console.log( `${index} : ${favouriteFruits[ index ]}`);
+
+
+
+
+}
